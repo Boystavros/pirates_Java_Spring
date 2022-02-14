1faee7604b567db8e3c6f090d01e3aeb0a4b79f5
Sacha Ponniah <sponniah812@gmail.com>
2022-02-14 17:41:12 +0000

    initial commit. MVP complete.

---
+++ src/main/java/com/codeclan/example/pirateservice/repositories/PirateRepository.java
@@ -0,0 +1,10 @@
+package com.codeclan.example.pirateservice.repositories;
+
+import com.codeclan.example.pirateservice.models.Pirate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PirateRepository extends JpaRepository<Pirate, Long> {
+
+
+
+}
